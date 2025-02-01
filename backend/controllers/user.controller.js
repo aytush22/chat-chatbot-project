ac33be9ff068650faa8262aaca6da89c21c1b5be
atyush22 <atyush22>
2025-02-01 21:06:31 +0545

    Upto reddis database

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import userModel from '../models/user.model.js';
 import * as userService from '../services/user.service.js';
 import { validationResult } from 'express-validator';
+// import redisClient from '../services/reddis.service.js';
 export const createUserController = async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) { //if errors not empty 
@@ -15,4 +16,41 @@ export const createUserController = async (req, res) => {
     catch (error) {
         res.status(400).send(error.message);
     }
+}
+
+export const loginController = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+
+    }
+    try {
+        const { email, password } = req.body;
+        const user = await userModel.findOne({ email }).select('+password');
+        if (!user) {
+            return res.status(401).json({
+                errors: "Invalid Credentials"
+            })
+        }
+
+        const isMatch = await user.isValidPassword(password);
+        if (!isMatch) {
+            return res.status(401).json({
+                errors: "Invalid credentials"
+            })
+        }
+
+        const token = await user.generateJWT();
+        res.status(200).json({ user, token });
+    }
+    catch (err) {
+        res.status(400).send(err.message);
+    }
+}
+
+export const profileController = async (req, res) => {
+    console.log(req.user);
+    res.status(200).json({
+        user: req.user
+    });
 }
