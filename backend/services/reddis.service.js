69267c78663ac52cabb7f5a6c0d3ce14a031e69e
atyush22 <atyush22>
2025-02-02 09:20:22 +0545

    connected reddis

--- backend/services/reddis.service.js
+++ backend/services/reddis.service.js
@@ -1,13 +1,13 @@
-// import Redis from "ioredis";
+import Redis from "ioredis";
 
-// const redisClient = new Redis({
-//     host: process.env.REDDIS_HOST,
-//     port: process.env.REDDIS_PORT,
-//     password: process.env.REDDIS_PASSWORD
-// });
+const redisClient = new Redis({
+    host: process.env.REDDIS_HOST,
+    port: process.env.REDDIS_PORT,
+    password: process.env.REDDIS_PASSWORD
+});
 
-// redisClient.on('connect', () => {
-//     console.log("Redis Connected");
-// });
+redisClient.on('connect', () => {
+    console.log("Redis Connected");
+});
 
-// export default redisClient;
+export default redisClient;
